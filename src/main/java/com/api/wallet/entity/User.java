382d5181c41407ab89d4bbf7584450eaeb6ddabf
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-11-17 01:50:39 -0300

    Criado User e adicionado jpa e lombok

---
+++ src/main/java/com/api/wallet/entity/User.java
@@ -0,0 +1,27 @@
+package com.api.wallet.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Data
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+}
